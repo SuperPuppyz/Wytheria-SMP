74a891b4f770921c88d95ec9517bb58a699bb7d7
Super Puppyz <133593815+SuperPuppyz@users.noreply.github.com>
2023-05-14 23:25:26 +0530

    correct the path

--- Modpack/build.sh
+++ Modpack/build.sh
@@ -1,3 +1,4 @@
+cd ModPack
 wget -O packwiz.zip https://nightly.link/packwiz/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip 
 apt-get install unzip
 unzip packwiz.zip
@@ -5,3 +6,4 @@ unzip packwiz.zip
 ./packwiz curseforge detect
 ./packwiz refresh
 ./packwiz curseforge export
+cd ..
