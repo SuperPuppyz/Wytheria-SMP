9ddc9a6e54d3350f1a8f0ca5f57c85978d462aef
Super Puppyz <133593815+SuperPuppyz@users.noreply.github.com>
2023-05-14 23:24:05 +0530

    Added build actions

--- Modpack/build.sh
+++ Modpack/build.sh
@@ -1,3 +1,7 @@
+wget -O packwiz.zip https://nightly.link/packwiz/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip 
+apt-get install unzip
+unzip packwiz.zip
+
 ./packwiz curseforge detect
 ./packwiz refresh
 ./packwiz curseforge export
